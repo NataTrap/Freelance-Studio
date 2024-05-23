73cd33a15dba1be0aedb38cee5ad127822c567e5
NataTrapaidze <ntrapaidze@icloud.com>
2024-05-23 15:56:09 +0400

    creating authorization and registration

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
                 { from: "./node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css", to: "css" },
                 { from: "./node_modules/admin-lte/dist/css/adminlte.min.css", to: "css" },
                 { from: "./node_modules/admin-lte/plugins/jquery/jquery.min.js", to: "js" },
-                { from: "./node_modules/admin-lte/dist/js/adminlte.min.js", to: "js" }
+                { from: "./node_modules/admin-lte/dist/js/adminlte.min.js", to: "js" },
+                { from: "./node_modules/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css", to: "css" },
             ],
         }),
     ],
